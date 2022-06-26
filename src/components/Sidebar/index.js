c51068fb59a8dce65b4828291058604205cd04e9
Ahmad Nagra <mhas@ciklum.com>
2022-06-26 22:44:57 +0500

    Updated animation

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoS from '../../assets/images/logo-n.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faUser, faHome } from '@fortawesome/free-solid-svg-icons'
@@ -12,7 +12,7 @@ const Sidebar = () => {
     <div className="nav-bar">
       <Link className="logo" to="/">
         <img src={LogoS} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="nagra" />
+        {/* <img className="sub-logo" src={LogoSubtitle} alt="nagra" /> */}
       </Link>
       <nav>
         <NavLink exact="true" activeclassname="active" to="/">
